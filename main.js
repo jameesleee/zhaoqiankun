ee7ac4216b371de9ab15299e8645380bc4231a96
Leeezy <342922856@qq.com>
2024-05-15 16:21:03 +1000

    no

--- main.js
+++ main.js
@@ -5,12 +5,8 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src=/zhao.jpg class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src=/sb.jpg class="logo vanilla" alt="JavaScript logo" />
-    </a>
+      <img src="/zhao.jpg" class="logo" alt="Vite logo" />
+      <img src="/sb.jpg" class="logo vanilla" alt="JavaScript logo" />
     <h1>傻逼赵乾坤</h1>
     <div class="card">
       <button id="counter" type="button"></button>
